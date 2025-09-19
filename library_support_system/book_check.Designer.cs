a51efc2b827c074829ee6cebfe254c9dcc57b2d2
mr-Choi07 <chltmdwo2007@naver.com>
2025-09-19 13:16:57 +0900

    Revert "병합을 위한 기존 디자인 삭제"
    This reverts commit 7d8f04b1eed3876891b9b06b4ae648c62d91a239.

---
+++ library_support_system/book_check.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace library_support_system
+{
+    partial class book_check
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // book_check
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1115, 627);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "book_check";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.book_check_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ library_support_system/book_check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library_support_system
+{
+    public partial class book_check : Form
+    {
+        public book_check()
+        {
+            InitializeComponent();
+        }
+
+        private void book_check_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
