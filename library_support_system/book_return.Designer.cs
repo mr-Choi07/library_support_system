1dadc82d1a477dd8662402c0e1b91919ea143696
mr-Choi07 <chltmdwo2007@naver.com>
2025-09-19 13:39:53 +0900

    화면 크기 맞품

--- library_support_system/book_return.Designer.cs
+++ library_support_system/book_return.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // book_return
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1256, 737);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "book_return";
             this.Text = "book_return";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
